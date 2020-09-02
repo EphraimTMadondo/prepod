b90c5102daf70a50c4f79f05c925253ae4f64590
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-02 12:53:57 +0200

    Added fresta theme files #2

--- application/controllers/Frest.php
+++ application/controllers/Frest.php
@@ -4,6 +4,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Frest extends CI_Controller{
     public function index()
     {
-        $this->load->view('frest/index.html');
+        $this->load->view('frest/html/ltr/vertical-menu-template-dark/index.html');
     }
 }
