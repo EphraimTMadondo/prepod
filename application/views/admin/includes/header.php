5dee85c43677dcbd1efcf03fe537b4593f90c788
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-02 17:13:54 +0200

    Changed head, header and main menu #17

--- application/views/admin/includes/header.php
+++ application/views/admin/includes/header.php
@@ -32,8 +32,12 @@ ob_start();
                   </ul>
                </div>
                <ul class="nav navbar-nav float-right">
-                  <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-us"></i><span class="selected-language">English</span></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown-flag"><a class="dropdown-item" href="#" data-language="en"><i class="flag-icon flag-icon-us mr-50"></i> English</a><a class="dropdown-item" href="#" data-language="fr"><i class="flag-icon flag-icon-fr mr-50"></i> French</a><a class="dropdown-item" href="#" data-language="de"><i class="flag-icon flag-icon-de mr-50"></i> German</a><a class="dropdown-item" href="#" data-language="pt"><i class="flag-icon flag-icon-pt mr-50"></i> Portuguese</a></div>
+                  <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-us"></i><span class="selected-language"><?php echo _l('language'); ?></span></a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown-flag">
+                           <?php foreach($this->app->get_available_languages() as $user_lang) { ?>
+                              <a class="dropdown-item" href="<?php echo admin_url('staff/change_language/'.$user_lang); ?>" data-language="en"><?php echo ucfirst($user_lang); ?></a>
+                           <?php } ?>
+                        </div>
                   </li>
                   <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand"><i class="ficon bx bx-fullscreen"></i></a></li>
                   <li class="nav-item nav-search"><a class="nav-link nav-link-search"><i class="ficon bx bx-search"></i></a>
