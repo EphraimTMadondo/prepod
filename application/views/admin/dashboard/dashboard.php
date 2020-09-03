f13eb041c04c34a159cd3db8ed3eabbbcde4521a
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-04 00:03:46 +0200

    Rearrange dashboard widgets

--- application/views/admin/dashboard/dashboard.php
+++ application/views/admin/dashboard/dashboard.php
@@ -32,6 +32,9 @@
                 <div class="col-md-8" data-container="left-8">
                     <?php render_dashboard_widgets('left-8'); ?>
                 </div>
+                <div class="col-md-8" data-container="left-8">
+                    <?php render_dashboard_widgets('left-8'); ?>
+                </div>
                 <div class="col-md-4" data-container="right-4">
                     <?php render_dashboard_widgets('right-4'); ?>
                 </div>
