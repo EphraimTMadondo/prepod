7587466812350e65fb8b564801de3ae71895f92c
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-03 14:09:11 +0200

    Todo widget #7
    Former-commit-id: a44f0b85b77fa0c026cee46672b2bd61d82034af

--- application/views/admin/dashboard/widgets/todos.php
+++ application/views/admin/dashboard/widgets/todos.php
@@ -16,14 +16,13 @@
                <ul class="widget-todo-list-wrapper" id="widget-todo-list">
                   <?php foreach($todos as $todo) { ?>
                   <li class="widget-todo-item">
-                     <?php echo form_hidden('todo_order',$todo['item_order']); ?>
-                     <?php echo form_hidden('finished',0); ?>
                      <div class="widget-todo-title-wrapper d-flex justify-content-between align-items-center mb-50">
                         <div class="widget-todo-title-area d-flex align-items-center">
-                           <div class="checkbox checkbox-shadow">
+                           <i class='bx bx-grid-vertical mr-25 font-medium-4 cursor-move'></i>
+                           <span class="checkbox checkbox-shadow">
                               <input type="checkbox" name="todo_id" id="<?php echo $todo['todoid']; ?>" class="checkbox__input" value="<?php echo $todo['todoid']; ?>">
                               <label for="<?php echo $todo['todoid']; ?>"></label>
-                           </div>
+                           </span>
                            <span class="widget-todo-title ml-50"><?php echo $todo['description']; ?></span>
                         </div>
                         <div class="widget-todo-item-action d-flex align-items-center">
@@ -32,22 +31,6 @@
                      </div>
                   </li>
                   <?php } ?>
-                  <li class="widget-todo-item">
-                     <div class="widget-todo-title-wrapper d-flex justify-content-between align-items-center mb-50">
-                        <div class="widget-todo-title-area d-flex align-items-center">
-                           <i class='bx bx-grid-vertical mr-25 font-medium-4 cursor-move'></i>
-                           <div class="checkbox checkbox-shadow">
-                                 <input type="checkbox" class="checkbox__input" id="checkbox1">
-                                 <label for="checkbox1"></label>
-                           </div>
-                           <span class="widget-todo-title ml-50">Add SCSS and JS files if
-                                 required</span>
-                        </div>
-                        <div class="widget-todo-item-action d-flex align-items-center">
-                           <i class="bx bx-dots-vertical-rounded font-medium-3 cursor-pointer"></i>
-                        </div>
-                     </div>
-                  </li>
                </ul>
             </div>
       </div>
