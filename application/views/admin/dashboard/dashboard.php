b9dcbf6bcb22c8b1a34475db3799c2da1d80b98f
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-03 12:29:01 +0200

    Financial Overview

--- application/views/admin/dashboard/dashboard.php
+++ application/views/admin/dashboard/dashboard.php
@@ -66,7 +66,7 @@
 
     <!-- BEGIN: Footer-->
     <footer class="footer footer-static footer-light">
-      <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; TBGA</span><span class="float-right d-sm-inline-block d-none">Crafted with<i class="bx bxs-heart pink mx-50 font-small-3"></i>by<a class="text-uppercase" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent</a></span>
+      <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; TBGA</span><span class="float-right d-sm-inline-block d-none">Designed with<i class="bx bxs-heart pink mx-50 font-small-3"></i>by<a class="text-uppercase" href="mailto:madondoephraim@gmail.com" target="_blank">Ephraim T Madondo</a></span>
         <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
       </p>
     </footer>
