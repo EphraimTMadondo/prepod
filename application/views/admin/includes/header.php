d72809dde5e7f75fc7c9f5602007f9a938085abe
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-03 10:19:05 +0200

    Changed head, header and main menu #18
    Former-commit-id: 30cd8e6a5cb86bc45d2e297b4627f5c10dc4d13d

--- application/views/admin/includes/header.php
+++ application/views/admin/includes/header.php
@@ -16,10 +16,9 @@ ob_start();
                         <li class="nav-item mobile-menu d-xl-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ficon bx bx-menu"></i></a></li>
                   </ul>
                   <ul class="nav navbar-nav bookmark-icons">
-                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email.html" data-toggle="tooltip" data-placement="top" title="Email"><i class="ficon bx bx-envelope"></i></a></li>
-                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat.html" data-toggle="tooltip" data-placement="top" title="Chat"><i class="ficon bx bx-chat"></i></a></li>
-                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Todo"><i class="ficon bx bx-check-circle"></i></a></li>
-                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-calendar.html" data-toggle="tooltip" data-placement="top" title="Calendar"><i class="ficon bx bx-calendar-alt"></i></a></li>
+                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="<?php echo base_url();?>admin/mailbox" data-toggle="tooltip" data-placement="top" title="Email"><i class="ficon bx bx-envelope"></i></a></li>
+                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="<?php echo base_url();?>admin/tasks" data-toggle="tooltip" data-placement="top" title="Todo"><i class="ficon bx bx-check-circle"></i></a></li>
+                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="<?php echo base_url();?>admin/calendar" data-toggle="tooltip" data-placement="top" title="Calendar"><i class="ficon bx bx-calendar-alt"></i></a></li>
                   </ul>
                   <ul class="nav navbar-nav">
                         <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i class="ficon bx bx-star warning"></i></a>
