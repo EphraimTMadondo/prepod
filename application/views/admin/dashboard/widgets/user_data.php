837426d19edd005fb3777cb75c042eb8a1e6c26b
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-04 11:16:29 +0200

    Rearrange dashboard widgets #events

--- application/views/admin/dashboard/widgets/user_data.php
+++ application/views/admin/dashboard/widgets/user_data.php
@@ -14,21 +14,21 @@
                <div class="widget-earnings-swiper swiper-container p-1">
                   <div class="swiper-wrapper">
                      <div class="swiper-slide rounded swiper-shadow py-75 px-2 d-flex align-items-center swiper-slide-active" id="home_tab_tasks">
-                           <i class="menu-livicon mr-50 font-large-1" data-icon="check-alt"></i>
+                           <i class="bx bx-task mr-50 font-large-1"></i>
                            <div class="swiper-text">
                               <?php echo _l('home_my_tasks'); ?>
                               <p class="mb-0 font-small-2 font-weight-normal"></p>
                            </div>
                      </div>
                      <div class="swiper-slide rounded swiper-shadow py-75 px-2 d-flex align-items-center" onclick="init_table_staff_projects(true);" id="home_my_projects">
-                           <i class="menu-livicon mr-50 font-large-1" data-icon="grid"></i>
+                           <i class="bx bx-briefcase mr-50 font-large-1"></i>
                            <div class="swiper-text">
                               <?php echo _l('home_my_projects'); ?>
                               <p class="mb-0 font-small-2 font-weight-normal"></p>
                            </div>
                      </div>
                      <div class="swiper-slide rounded swiper-shadow py-75 px-2 d-flex align-items-center"  onclick="initDataTable('.table-my-reminders', admin_url + 'misc/my_reminders', undefined, undefined,undefined,[2,'asc']);" id="home_my_reminders">
-                           <i class="menu-livicon mr-50 font-large-1" data-icon="alarm"></i>
+                           <i class="bx bx-alarm mr-50 font-large-1"></i>
                            <div class="swiper-text">
                               <?php echo _l('my_reminders'); ?>
                               <p class="mb-0 font-small-2 font-weight-normal">
@@ -49,7 +49,7 @@
                      </div>
                      <?php if((get_option('access_tickets_to_none_staff_members') == 1 && !is_staff_member()) || is_staff_member()){ ?>
                         <div class="swiper-slide rounded swiper-shadow py-75 px-2 d-flex align-items-center"  onclick="init_table_tickets(true);" id="home_tab_tickets">
-                              <i class="menu-livicon mr-50 font-large-1" data-icon="priority-high"></i>
+                              <i class="bx bx-support mr-50 font-large-1"></i>
                               <div class="swiper-text">
                                  <?php echo _l('home_tickets'); ?>
                                  <p class="mb-0 font-small-2 font-weight-normal"></p>
@@ -58,7 +58,7 @@
                      <?php } ?>
                      <?php if(is_staff_member()){ ?>
                         <div class="swiper-slide rounded swiper-shadow py-75 px-2 d-flex align-items-center"  onclick="init_table_announcements(true);" id="home_announcements">
-                              <i class="menu-livicon mr-50 font-large-1" data-icon="bell"></i>
+                              <i class="bx bx-notification mr-50 font-large-1"></i>
                               <div class="swiper-text">
                                  <?php echo _l('home_announcements'); ?>
                                  <p class="mb-0 font-small-2 font-weight-normal"><?php if($total_undismissed_announcements != 0){ echo $total_undismissed_announcements;} ?></p>
@@ -67,7 +67,7 @@
                      <?php } ?>
                      <?php if(is_admin()){ ?>
                         <div class="swiper-slide rounded swiper-shadow py-75 px-2 d-flex align-items-center" id="home_tab_activity">
-                              <i class="menu-livicon mr-50 font-large-1" data-icon="timer"></i>
+                              <i class="bx bx-time mr-50 font-large-1"></i>
                               <div class="swiper-text">
                                  <?php echo _l('home_latest_activity'); ?>
                                  <p class="mb-0 font-small-2 font-weight-normal"></p>
