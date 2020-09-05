1e4d0dafca5dc5d22491044e064077be63468bb8
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-05 11:44:09 +0200

    styles template chnaged manage.php
    Former-commit-id: 032959cb3246f7a4f642ee2a4d09fbca55f04605

--- application/views/admin/tasks/manage.php
+++ application/views/admin/tasks/manage.php
@@ -3,27 +3,27 @@
    ini_set('display_errors', 1);
 ?>
 
-   <?php
-   if($this->session->has_userdata('tasks_kanban_view') && $this->session->userdata('tasks_kanban_view') == 'true') { 
-         init_head('task_kanban');
-      } else { 
-         init_head('task_list');
-   }?>
+<?php
+if($this->session->has_userdata('tasks_kanban_view') && $this->session->userdata('tasks_kanban_view') == 'true') { 
+      init_head(true, 'task_kanban');
+   } else { 
+      init_head(true, 'task_list');
+}?>
 
-   <!-- BEGIN: Content-->
-   <?php
-   if($this->session->has_userdata('tasks_kanban_view') && $this->session->userdata('tasks_kanban_view') == 'true') { 
-         $this->load->view("admin/tasks/tasks_kanban.php");
-      } else { 
-         $this->load->view("admin/tasks/tasks_list.php");
-   }?>
-   <!-- END: Content-->
+<!-- BEGIN: Content-->
+<?php
+if($this->session->has_userdata('tasks_kanban_view') && $this->session->userdata('tasks_kanban_view') == 'true') { 
+      $this->load->view("admin/tasks/tasks_kanban.php");
+   } else { 
+      $this->load->view("admin/tasks/tasks_list.php");
+}?>
+<!-- END: Content-->
 
-   <?php
-   if($this->session->has_userdata('tasks_kanban_view') && $this->session->userdata('tasks_kanban_view') == 'true') { 
-         init_tail('task_kanban');
-      } else { 
-         init_tail('task_list');
-   }?>
-   </body>
+<?php
+if($this->session->has_userdata('tasks_kanban_view') && $this->session->userdata('tasks_kanban_view') == 'true') { 
+      init_tail('task_kanban');
+   } else { 
+      init_tail('task_list');
+}?>
+</body>
 </html>
