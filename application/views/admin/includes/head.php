5080f71bf02b071dcea04a478e7f5b2eeb045f5e
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-05 16:31:39 +0200

    task list styles added app-todo.css

--- application/views/admin/includes/head.php
+++ application/views/admin/includes/head.php
@@ -75,12 +75,12 @@
         case "task_list":
           echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/forms/select/select2.min.css'>\n\t";
           echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/editors/quill/quill.snow.css'>\n\t";
-          echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/pages/app-todo.css\n\t";
+          echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/pages/app-todo.css>\n\t";
         break;
         case "task_kanban":
           echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/forms/select/select2.min.css'>\n\t";
           echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/editors/quill/quill.snow.css'>\n\t";
-          echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/pages/app-kanban.css\n\t";
+          echo "<link rel='stylesheet' type='text/css' href='".base_url()."assets/frest/app-assets/vendors/css/pages/app-kanban.css>\n\t";
         break;
       }
     ?>
