cdb2ec844bb222d202554d0f6c56ea25adaad0d2
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-07 21:17:33 +0200

    Inner pages mailbox functionality #Content
    Former-commit-id: 7ea5c0be2d73602f3391fa687a33df2dc2d4fcae

--- modules/mailbox/views/table_outbox.php
+++ modules/mailbox/views/table_outbox.php
@@ -40,7 +40,9 @@ foreach ($rResult as $aRow) {
         $has_attachment = '<i class="bx bx-paperclip mr-50"></i>';
     }
 
-    echo '<li class="media" onclick="location.href="'.admin_url().'mailbox/outbox/'.$aRow['id'].'";">
+    $redirectTO = "location.href='".admin_url()."mailbox/inbox/".$aRow['id']."';";
+
+    echo '<li class="media" onclick="'.$redirectTO.'">
         <div class="user-action">
             <div class="checkbox-con mr-25">
                 <div style="margin-top:-10" class="checkbox checkbox-shadow checkbox-sm">
