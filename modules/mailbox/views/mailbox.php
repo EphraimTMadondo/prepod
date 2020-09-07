837c694acaa20ec6de2897b169ebc4c4185611dd
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-07 11:16:31 +0200

    Inner pages mailbox functionality #Content
    Former-commit-id: 62c0e7b1c24f0052f7f354e19507592fe93595b4

--- modules/mailbox/views/mailbox.php
+++ modules/mailbox/views/mailbox.php
@@ -687,7 +687,7 @@
 
     $(function(){
         //Added by Ephraim
-        fetch(admin_url + 'mailbox/table/<?php echo $group;?>)
+        fetch(admin_url + 'mailbox/table/<?php echo $group;?>')
         .then(result => result.json())
         .then(data => console.log(data));
     });
