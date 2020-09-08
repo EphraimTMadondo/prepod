14187c3e0583c461781228b4f6f122294f7f968b
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-08 13:09:58 +0200

    Inner pages mailbox functionality #Content
    Former-commit-id: dade6cc5d76f0a8b2d671d644a5aef1c9099f75b

--- assets/frest/app-assets/js/scripts/pages/app-email.js
+++ assets/frest/app-assets/js/scripts/pages/app-email.js
@@ -194,9 +194,9 @@ $(function () {
         email_app_details.find('#mail-date').html(mail.inbox.date_sent);
         email_app_details.find('#profile-image').html(mail.staff_profile_image);
         email_app_details.find('#sender-name').html(mail.inbox.sender_name);
-        email_app_details.find('#staff-email').html("&lt;"+ mail.staff_email +"&gt;");
-        email_app_details.find('#to').html'to '+(mail.inbox.to);
-        email_app_details.find('#cc').html('cc '+mail.inbox.cc);
+        email_app_details.find('#staff-email').html("&lt;" + mail.staff_email + "&gt;");
+        email_app_details.find('#to').html('to ' + mail.inbox.to);
+        email_app_details.find('#cc').html('cc ' + mail.inbox.cc);
         if(mail.inbox.has_attachment){
             mail.attachments.forEach(attachment => {
               email_app_details.find('#attachment-list').append('<li class="cursor-pointer">\n'+
