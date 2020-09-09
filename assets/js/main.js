b29f1c4bf1272697e24257d253c2a81e77f1adf1
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-09 21:15:42 +0200

    Datatables
    Former-commit-id: 7ad3d7db2e9c8813e8f9344fc827e3f6c5211dca

--- assets/js/main.js
+++ assets/js/main.js
@@ -2667,6 +2667,7 @@ function initDataTable(selector, url, notsearchable, notsortable, fnserverparams
         "bDeferRender": true,
         "responsive": true,
         "autoWidth": false,
+        dom: "lBfrti",
         "pageLength": app.options.tables_pagination_limit,
         "lengthMenu": [length_options, length_options_names],
         "columnDefs": [{
