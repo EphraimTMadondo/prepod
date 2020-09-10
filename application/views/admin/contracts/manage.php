04d4a75497740111307f64d0012751d7e776d8c1
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-10 16:53:49 +0200

    Pages Layout #leads

--- application/views/admin/contracts/manage.php
+++ application/views/admin/contracts/manage.php
@@ -3,7 +3,8 @@
 
 <!-- BEGIN: Content-->
 <div class="app-content content">
-   <div class="content-overlay"></div>
+    <div class="content-overlay"></div>
+    <div class="content-wrapper">
         <div class="row">
             <div class="col-md-12">
                 <div class="card mtop20">
