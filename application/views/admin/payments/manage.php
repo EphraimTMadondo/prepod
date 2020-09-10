d8e391fd3a33d6ecad8447cb4468ebfd51267ca5
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-10 17:21:39 +0200

    Pages Layout #invoices

--- application/views/admin/payments/manage.php
+++ application/views/admin/payments/manage.php
@@ -1,9 +1,9 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php init_head(); ?>
-
 <!-- BEGIN: Content-->
 <div class="app-content content">
-   <div class="content-overlay"></div>
+	<div class="content-overlay"></div>
+	<div class="content-wrapper">
 		<div class="card mtop20">
 			<div class="card-body">
 				<?php $this->load->view('admin/payments/table_html'); ?>
