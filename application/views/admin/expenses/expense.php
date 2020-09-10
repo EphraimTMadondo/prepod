26bf9d22bf0dc0515fd3d990bbdd0b29eb5579d2
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-10 17:00:24 +0200

    Pages Layout #leads
    Former-commit-id: f4a53f075802c678e2133f0c184f2e19076e2c37

--- application/views/admin/expenses/expense.php
+++ application/views/admin/expenses/expense.php
@@ -1,9 +1,9 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php init_head(); ?>
-
 <!-- BEGIN: Content-->
 <div class="app-content content">
    <div class="content-overlay"></div>
+   <div class="content-wrapper">
       <div class="row">
          <?php
             if(isset($expense)){
