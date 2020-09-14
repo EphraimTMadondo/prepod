321313912048299f014dc518b549f292882aea57
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-14 13:22:03 +0200

    Inner pages assets
    Former-commit-id: 381f6267bdd1823119a5d1395aeb506e8b3308ee

--- modules/assets/views/manage_assets.php
+++ modules/assets/views/manage_assets.php
@@ -11,6 +11,7 @@
                      <div class="col-md-4 border-right">
                       <h4 class="no-margin font-bold"><i class="fa fa-bank" aria-hidden="true"></i> <?php echo _l($title); ?></h4>
                       <hr />
+                      <a href="#" class="btn btn-light float-right btn-with-tooltip toggle-small-view hidden-xs" onclick="toggle_small_view_asset('.asset_sm','#asset_sm_view'); return false;" data-toggle="tooltip" title="<?php echo htmlspecialchars(_l('invoices_toggle_table_tooltip')); ?>"><i class="fa fa-angle-double-left"></i></a>
                     </div>
                   </div>
                   <div class="row">    
@@ -67,7 +68,6 @@
                     </li>
                 </ul>
            </div>
-           <a href="#" class="btn btn-light float-right btn-with-tooltip toggle-small-view hidden-xs" onclick="toggle_small_view_asset('.asset_sm','#asset_sm_view'); return false;" data-toggle="tooltip" title="<?php echo htmlspecialchars(_l('invoices_toggle_table_tooltip')); ?>"><i class="fa fa-angle-double-left"></i></a>
          </div>
        </div>
      </div>
