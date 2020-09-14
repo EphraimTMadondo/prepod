d9675ef9f506507bcb5420a746f79bcd99830396
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-14 13:28:51 +0200

    Inner pages assets
    Former-commit-id: 95a1ba697efb4b9ed8387543f4ec362407e239bc

--- modules/assets/views/manage_assets.php
+++ modules/assets/views/manage_assets.php
@@ -10,13 +10,13 @@
                   <div class="row">
                      <div class="col-md-12">
                       <h4 class="no-margin font-bold"><i class="fa fa-bank" aria-hidden="true"></i> <?php echo _l($title); ?></h4>
-                      <hr />
-                      <a href="#" class="btn btn-light float-right btn-with-tooltip toggle-small-view hidden-xs" onclick="toggle_small_view_asset('.asset_sm','#asset_sm_view'); return false;" data-toggle="tooltip" title="<?php echo htmlspecialchars(_l('invoices_toggle_table_tooltip')); ?>"><i class="fa fa-angle-double-left"></i></a>
                       <?php if (has_permission('assets', '', 'create') || is_admin()) { ?>
                         <a href="#" onclick="new_asset(); return false;" class="btn btn-primary float-right ml-1">
                             <?php echo _l('new_asset'); ?>
                         </a>
                     	<?php } ?>
+                      <a href="#" class="btn btn-light float-right btn-with-tooltip toggle-small-view hidden-xs" onclick="toggle_small_view_asset('.asset_sm','#asset_sm_view'); return false;" data-toggle="tooltip" title="<?php echo htmlspecialchars(_l('invoices_toggle_table_tooltip')); ?>"><i class="fa fa-angle-double-left"></i></a>
+                      <hr />
                     </div>
                   </div>
                   <ul class="nav nav-tabs mt-2" role="tablist">
