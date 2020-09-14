28270e6ca35713898d2fe95e1cf5d3e2054633d9
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-14 11:26:42 +0200

    Inner pages invoice_items
    Former-commit-id: d764e06f0f378a76aecc91f3484845501ff61751

--- application/views/admin/projects/manage.php
+++ application/views/admin/projects/manage.php
@@ -6,93 +6,93 @@
 	<div class="content-wrapper">
     <div class="row">
       <div class="col-md-12">
-            <div class="card mt-2">
-              <div class="card-body">
-              <div class="_buttons">
-              <?php if(has_permission('projects','','create')){ ?>
-                <a href="<?php echo admin_url('projects/project'); ?>" class="btn btn-primary float-left mr-5">
-                  <?php echo _l('new_project'); ?>
-                </a>
-              <?php } ?>
-              <a href="<?php echo admin_url('projects/gantt'); ?>" data-toggle="tooltip" title="<?php echo _l('project_gant'); ?>" class="btn btn-light"><i class="fa fa-align-left" aria-hidden="true"></i></a>
-              <div class="btn-group float-right ml-1 btn-with-tooltip-group _filter_data" data-toggle="tooltip" data-title="<?php echo _l('filter_by'); ?>">
-                <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="fa fa-filter" aria-hidden="true"></i>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-right width300">
-                  <li>
-                    <a href="#" data-cview="all" onclick="dt_custom_view('','.table-projects',''); return false;">
-                      <?php echo _l('expenses_list_all'); ?>
-                    </a>
-                  </li>
-                  <?php
-                  // Only show this filter if user has permission for projects view otherwise wont need this becuase by default this filter will be applied
-                  if(has_permission('projects','','view')){ ?>
-                  <li>
-                    <a href="#" data-cview="my_projects" onclick="dt_custom_view('my_projects','.table-projects','my_projects'); return false;">
-                      <?php echo _l('home_my_projects'); ?>
+        <div class="card mt-2">
+          <div class="card-body">
+            <div class="_buttons">
+            <?php if(has_permission('projects','','create')){ ?>
+              <a href="<?php echo admin_url('projects/project'); ?>" class="btn btn-primary float-right">
+                <?php echo _l('new_project'); ?>
+              </a>
+            <?php } ?>
+            <a href="<?php echo admin_url('projects/gantt'); ?>" data-toggle="tooltip" title="<?php echo _l('project_gant'); ?>" class="btn btn-light"><i class="fa fa-align-left" aria-hidden="true"></i></a>
+            <div class="btn-group float-left btn-with-tooltip-group _filter_data" data-toggle="tooltip" data-title="<?php echo _l('filter_by'); ?>">
+              <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="bx bx-filter" aria-hidden="true"></i>
+              </button>
+              <ul class="dropdown-menu dropdown-menu-right width300">
+                <li>
+                  <a href="#" data-cview="all" onclick="dt_custom_view('','.table-projects',''); return false;">
+                    <?php echo _l('expenses_list_all'); ?>
+                  </a>
+                </li>
+                <?php
+                // Only show this filter if user has permission for projects view otherwise wont need this becuase by default this filter will be applied
+                if(has_permission('projects','','view')){ ?>
+                <li>
+                  <a href="#" data-cview="my_projects" onclick="dt_custom_view('my_projects','.table-projects','my_projects'); return false;">
+                    <?php echo _l('home_my_projects'); ?>
+                  </a>
+                </li>
+                <?php } ?>
+                <li class="divider"></li>
+                <?php foreach($statuses as $status){ 
+                
+              
+                ?>
+                  <li class="<?php if($status['filter_default'] == true && !$this->input->get('status') || $this->input->get('status') == $status['id']){echo 'active';} ?>">
+                    <a href="#" data-cview="<?php echo 'project_status_'.$status['id']; ?>" onclick="dt_custom_view('project_status_<?php echo $status['id']; ?>','.table-projects','project_status_<?php echo $status['id']; ?>'); return false;">
+                      <?php echo $status['name']; ?>
                     </a>
                   </li>
                   <?php } ?>
-                  <li class="divider"></li>
-                  <?php foreach($statuses as $status){ 
-                  
-                
-                  ?>
-                    <li class="<?php if($status['filter_default'] == true && !$this->input->get('status') || $this->input->get('status') == $status['id']){echo 'active';} ?>">
-                      <a href="#" data-cview="<?php echo 'project_status_'.$status['id']; ?>" onclick="dt_custom_view('project_status_<?php echo $status['id']; ?>','.table-projects','project_status_<?php echo $status['id']; ?>'); return false;">
-                        <?php echo $status['name']; ?>
-                      </a>
-                    </li>
-                    <?php } ?>
-                  </ul>
-                </div>
-                <div class="clearfix"></div>
-                <hr class="hr-panel-heading" />
+                </ul>
               </div>
-               <div class="row mbot15">
-                <div class="col-md-12">
-                  <h4 class="no-margin"><?php echo _l('projects_summary'); ?></h4>
-                  <?php
-                  $_where = '';
-                  if(!has_permission('projects','','view')){
-                    $_where = 'id IN (SELECT project_id FROM '.db_prefix().'project_members WHERE staff_id='.get_staff_user_id().')';
-                  }
-                  ?>
-                </div>
-                <div class="col-md-12 _filters _hidden_inputs center">
-                  <?php
-                  echo form_hidden('my_projects');
-                  foreach($statuses as $status){
-                   $value = $status['id'];
-                     if($status['filter_default'] == false && !$this->input->get('status')){
-                        $value = '';
-                     } else if($this->input->get('status')) {
-                        $value = ($this->input->get('status') == $status['id'] ? $status['id'] : "");
-                     }
-                     echo form_hidden('project_status_'.$status['id'],$value);
-                    ?>
-                    <?
-                                $companyusername = $_SESSION['current_company'];
-                    ?>
-                   <div class="col-md-2 col-xs-6 border-right">
-                    <?php $where = ($_where == '' ? '' : $_where.' AND ').'status = '.$status['id']." AND company_username = '$companyusername'"; ?>
-
-                    <a href="#" onclick="dt_custom_view('project_status_<?php echo $status['id']; ?>','.table-projects','project_status_<?php echo $status['id']; ?>',true); return false;">
-                     <h3 class="bold"><?php echo total_rows(db_prefix().'projects',$where); ?></h3>
-                     <span style="color:<?php echo $status['color']; ?>" project-status-<?php echo $status['id']; ?>">
-                     <?php echo $status['name']; ?>
-                     </span>
-                   </a>
-                 </div>
-                 <?php } ?>
-               </div>
-             </div>
-             <div class="clearfix"></div>
+              <div class="clearfix"></div>
               <hr class="hr-panel-heading" />
-             <?php echo form_hidden('custom_view'); ?>
-             <?php $this->load->view('admin/projects/table_html'); ?>
-          </div>
+            </div>
+              <div class="row mbot15">
+              <div class="col-md-12">
+                <h4 class="no-margin"><?php echo _l('projects_summary'); ?></h4>
+                <?php
+                $_where = '';
+                if(!has_permission('projects','','view')){
+                  $_where = 'id IN (SELECT project_id FROM '.db_prefix().'project_members WHERE staff_id='.get_staff_user_id().')';
+                }
+                ?>
+              </div>
+              <div class="row _filters _hidden_inputs center">
+                <?php
+                echo form_hidden('my_projects');
+                foreach($statuses as $status){
+                  $value = $status['id'];
+                    if($status['filter_default'] == false && !$this->input->get('status')){
+                      $value = '';
+                    } else if($this->input->get('status')) {
+                      $value = ($this->input->get('status') == $status['id'] ? $status['id'] : "");
+                    }
+                    echo form_hidden('project_status_'.$status['id'],$value);
+                  ?>
+                  <?
+                              $companyusername = $_SESSION['current_company'];
+                  ?>
+                  <div class="col-md-2 col-xs-6 border-right">
+                  <?php $where = ($_where == '' ? '' : $_where.' AND ').'status = '.$status['id']." AND company_username = '$companyusername'"; ?>
+
+                  <a href="#" onclick="dt_custom_view('project_status_<?php echo $status['id']; ?>','.table-projects','project_status_<?php echo $status['id']; ?>',true); return false;">
+                    <h3 class="bold"><?php echo total_rows(db_prefix().'projects',$where); ?></h3>
+                    <span style="color:<?php echo $status['color']; ?>" project-status-<?php echo $status['id']; ?>">
+                    <?php echo $status['name']; ?>
+                    </span>
+                  </a>
+                </div>
+                <?php } ?>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+            <hr class="hr-panel-heading" />
+            <?php echo form_hidden('custom_view'); ?>
+            <?php $this->load->view('admin/projects/table_html'); ?>
+        </div>
         </div>
       </div>
     </div>
