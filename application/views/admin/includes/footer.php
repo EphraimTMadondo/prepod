2073bbfe43c15532915af9f49c339442c48e6d1c
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-15 15:19:28 +0200

    Inner pages footer

--- application/views/admin/includes/footer.php
+++ application/views/admin/includes/footer.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; TBGA</span><span class="float-right d-sm-inline-block d-none">Designed with<i class="bx bxs-heart pink mx-50 font-small-3"></i>by<a class="text-uppercase" href="mailto:madondoephraim@gmail.com" target="_blank">Ephraim T Madondo</a></span>
+    <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; Worksuite</span><span class="float-right d-sm-inline-block d-none">Designed with<i class="bx bxs-heart pink mx-50 font-small-3"></i>by<a class="text-uppercase" href="mailto:madondoephraim@gmail.com" target="_blank">TBGA</a></span>
     <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
     </p>
 </footer>
