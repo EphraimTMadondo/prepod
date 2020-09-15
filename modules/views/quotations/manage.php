373052cefd4ac923669665b334ecd1f4992a36bf
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-15 12:29:53 +0200

    Bulk edits #mright5, mright10, mright15 => mr-1 #mright25 => mr-2 #mleft5, mleft10, mleft15 =M ml-1 #panel-body => card-body # panel_s => card #<div id=wrapper> =>

--- modules/views/quotations/manage.php
+++ modules/views/quotations/manage.php
@@ -1,7 +1,9 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php init_head(); ?>
-<div id="wrapper">
-	<div class="content">
+<!-- BEGIN: Content-->
+<div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="content-wrapper">
 		<div class="row">
 			<?php $this->load->view('quotations/list_template'); ?>
 		</div>
