9cb0852538368b59abaeef1bf776002f22fd0ac7
itsvictorj <janhivictor4@gmail.com>
2020-09-16 12:08:49 +0200

    returned class

--- application/views/themes/assets/js/my-calendar.js
+++ application/views/themes/assets/js/my-calendar.js
@@ -224,10 +224,7 @@
     var end = event.end ? new Date(event.end.getTime()) : moment().add(1, 'hours').toDate();
 
     if (useCreationPopup) {
-      cal.openCreationPopup({
-        start: start,
-        end: end
-      });
+      
     }
   }
   // new schedule create
