de6803581591b7087c0dbd2691173b2feff1707e
itsvictorj <janhivictor4@gmail.com>
2020-09-16 10:59:39 +0200

    Removed modal..testing

--- application/views/admin/utilities/calendar.php
+++ application/views/admin/utilities/calendar.php
@@ -127,31 +127,12 @@
 	</div>
 </div>
 
-<!-- Modal -->
-<div id="createScheduleModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
 
 
 <?php $this->load->view('admin/utilities/calendar_template'); ?>
 <script>
-	app.calendarIDs = '<?php echo json_encode($google_ids_calendars); ?>';
+//	app.calendarIDs = '<?php echo json_encode($google_ids_calendars); ?>';
 </script>
 <?php init_tail('calendar'); ?>
 <script>
