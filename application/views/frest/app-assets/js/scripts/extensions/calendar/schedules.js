70c8abac6b6bf8b336e31daa1380a1a1e842fc93
itsvictorj <janhivictor4@gmail.com>
2020-09-17 13:28:18 +0200

    removed generate

--- application/views/frest/app-assets/js/scripts/extensions/calendar/schedules.js
+++ application/views/frest/app-assets/js/scripts/extensions/calendar/schedules.js
@@ -91,71 +91,9 @@ function generateTime(schedule, renderStart, renderEnd) {
 }
 // randome name generate
 function generateNames() {
-  var names = [];
-  var i = 0;
-  var length = chance.integer({ min: 1, max: 10 });
 
-  for (i; i < length; i += 1) {
-    names.push(chance.name({ nationality: 'en' }));
-  }
-  return names;
-}
-// generate randome schedule
-function generateRandomSchedule(calendar, renderStart, renderEnd) {
-  var schedule = new ScheduleInfo();
-
-  schedule.id = chance.guid();
-  schedule.calendarId = calendar.id;
-
-  schedule.title = chance.name({ nationalty: 'en' });
-  schedule.body = chance.bool({ likelihood: 20 }) ? chance.sentence({ words: 10 }) : '';
-  schedule.isReadOnly = chance.bool({ likelihood: 20 });
-  generateTime(schedule, renderStart, renderEnd);
-
-  schedule.isPrivate = chance.bool({ likelihood: 10 });
-  schedule.location = chance.address();
-  schedule.attendees = chance.bool({ likelihood: 70 }) ? generateNames() : [];
-  schedule.recurrenceRule = chance.bool({ likelihood: 20 }) ? 'repeated events' : '';
-
-  schedule.color = calendar.color;
-  schedule.bgColor = calendar.bgColor;
-  schedule.dragBgColor = calendar.dragBgColor;
-  schedule.borderColor = calendar.borderColor;
-
-  if (schedule.category === 'milestone') {
-    schedule.color = schedule.bgColor;
-    schedule.bgColor = 'transparent';
-    schedule.dragBgColor = 'transparent';
-    schedule.borderColor = 'transparent';
-  }
-
-  schedule.raw.memo = chance.sentence();
-  schedule.raw.creator.name = chance.name();
-  schedule.raw.creator.avatar = chance.avatar();
-  schedule.raw.creator.company = chance.company();
-  schedule.raw.creator.email = chance.email();
-  schedule.raw.creator.phone = chance.phone();
-
-  if (chance.bool({ likelihood: 20 })) {
-    var travelTime = chance.minute();
-    schedule.goingDuration = travelTime;
-    schedule.comingDuration = travelTime;
-  }
-
-  ScheduleList.push(schedule);
 }
 // random schedule created
 function generateSchedule(viewName, renderStart, renderEnd) {
-  ScheduleList = [];
-  CalendarList.forEach(function (calendar) {
-    var i = 0, length = 1;
-    if (viewName === 'month') {
-      length = 1;
-    } else if (viewName === 'day') {
-      length = 2;
-    }
-    for (i; i < length; i += 1) {
-      generateRandomSchedule(calendar, renderStart, renderEnd);
-    }
-  });
+
 }
