43f32fc4dd7f16a0f45969335b37bafa2a45a3ae
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-17 16:54:55 +0200

    Inner Pages proposals

--- application/views/admin/contracts/manage.php
+++ application/views/admin/contracts/manage.php
@@ -154,9 +154,7 @@
                                     </div>
                                 </div>
                                 <?php echo form_hidden('custom_view'); ?>
-                                <div class="clearfix"></div>
-                                <hr class="hr-panel-heading" />
-                                <div class="col-md-12">
+                                <div class="col-md-12 border-top mt-1">
                                     <?php $this->load->view('admin/contracts/table_html'); ?>
                                 </div>
                             </div>
