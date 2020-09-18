f1d2897d433be1cd115e3ab4fde2d4214cde9a13
itsvictorj <janhivictor4@gmail.com>
2020-09-18 10:52:53 +0200

    added echo in model fpr test and removed alert errpr
    Former-commit-id: b68fcd6e4257f45861e4bbf956a2a006f0b56a96

--- assets/frest/app-assets/js/scripts/extensions/calendar/app-calendar.js
+++ assets/frest/app-assets/js/scripts/extensions/calendar/app-calendar.js
@@ -377,7 +377,7 @@ var ScheduleList = [];
         success: function(result){
               result1 = result;
             //$("#div1").html(result);
-            alert("Leo " + result);
+           // alert("Leo " + result);
            // setSchedules2(JSON.parse(result));
             
         },
