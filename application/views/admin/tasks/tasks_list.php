a2feae1e6e1c7fc421e5d3814ca9e9c0f25b7a9e
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-18 11:34:30 +0200

    Inner Pages proposals kanban

--- application/views/admin/tasks/tasks_list.php
+++ application/views/admin/tasks/tasks_list.php
@@ -1,25 +1,5 @@
 <!-- BEGIN: Content-->
 <div class="app-content content">
-    <div class="content-header row">
-        <div class="col-md-8">
-            <?php if(has_permission('tasks','','create')){ ?>
-            <a href="#" onclick="new_task(<?php if($this->input->get('project_id')){ echo "'".admin_url('tasks/task?rel_id='.$this->input->get('project_id').'&rel_type=project')."'";} ?>); return false;" class="btn btn-info pull-left new"><?php echo _l('new_task'); ?></a>
-            <?php } ?>
-            <a href="<?php if(!$this->input->get('project_id')){ echo admin_url('tasks/switch_kanban/'.$switch_kanban); } else { echo admin_url('projects/view/'.$this->input->get('project_id').'?group=project_tasks'); }; ?>" class="btn btn-default mleft10 pull-left hidden-xs">
-                <?php if($switch_kanban == 1){ echo _l('switch_to_list_view');}else{echo _l('leads_switch_to_kanban');}; ?>
-            </a>
-        </div>
-        <div class="col-md-4">
-            <?php if($this->session->has_userdata('tasks_kanban_view') && $this->session->userdata('tasks_kanban_view') == 'true') { ?>
-            <div data-toggle="tooltip" data-placement="bottom" data-title="<?php echo _l('search_by_tags'); ?>">
-                <?php echo render_input('search','','','search',array('data-name'=>'search','onkeyup'=>'tasks_kanban();','placeholder'=>_l('search_tasks')),array(),'no-margin') ?>
-            </div>
-            <?php } else { ?>
-            <?php $this->load->view('admin/tasks/tasks_filter_by',array('view_table_name'=>'.table-tasks')); ?>
-            <a href="<?php echo admin_url('tasks/detailed_overview'); ?>" class="btn btn-success pull-right mright5"><?php echo _l('detailed_overview'); ?></a>
-            <?php } ?>
-        </div>
-    </div>
     <div class="content-area-wrapper">
         <div class="sidebar-left">
             <div class="sidebar">
@@ -31,10 +11,13 @@
                     <div class="todo-app-menu">
                         <div class="form-group text-center add-task">
                             <!-- new task button -->
-                            <button type="button" class="btn btn-primary add-task-btn btn-block my-1">
+                            <button type="button" class="btn btn-primary add-task-btn my-1">
                                 <i class="bx bx-plus"></i>
                                 <span>New Task</span>
                             </button>
+                            <a href="<?php if(!$this->input->get('project_id')){ echo admin_url('tasks/switch_kanban/'.$switch_kanban); } else { echo admin_url('projects/view/'.$this->input->get('project_id').'?group=project_tasks'); }; ?>" class="btn btn-light hidden-xs">
+                                <?php if($switch_kanban == 1){ echo _l('switch_to_list_view');}else{echo _l('leads_switch_to_kanban');}; ?>
+                            </a>
                         </div>
                         <!-- sidebar list start -->
                         <div class="sidebar-menu-list">
