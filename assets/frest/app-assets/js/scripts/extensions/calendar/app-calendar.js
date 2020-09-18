8dbd1098aeb71ad62bfb05fb0b41a8c36766b627
itsvictorj <janhivictor4@gmail.com>
2020-09-18 11:43:58 +0200

    added getMySchedules
    Former-commit-id: ee4e4ff57bd7be364c988267fa16115d0dcdea47

--- assets/frest/app-assets/js/scripts/extensions/calendar/app-calendar.js
+++ assets/frest/app-assets/js/scripts/extensions/calendar/app-calendar.js
@@ -346,6 +346,9 @@ var ScheduleList = [];
       } else if (viewName === 'day') {
         length = 2;
       }
+
+      getMySchedules();
+      
       for (i; i < length; i += 1) {
         generateRandomSchedule(calendar, renderStart, renderEnd);
       }
@@ -353,6 +356,52 @@ var ScheduleList = [];
   }
 
   function generateRandomSchedule(calendar, renderStart, renderEnd) {
+
+    var schedule = new ScheduleInfo();
+
+    schedule.id = chance.guid();
+    schedule.calendarId = calendar.id;
+  
+    schedule.title = chance.name({ nationalty: 'en' });
+    schedule.body = chance.bool({ likelihood: 20 }) ? chance.sentence({ words: 10 }) : '';
+    schedule.isReadOnly = chance.bool({ likelihood: 20 });
+    generateTime(schedule, renderStart, renderEnd);
+  
+    schedule.isPrivate = chance.bool({ likelihood: 10 });
+    schedule.location = chance.address();
+    schedule.attendees = chance.bool({ likelihood: 70 }) ? generateNames() : [];
+    schedule.recurrenceRule = chance.bool({ likelihood: 20 }) ? 'repeated events' : '';
+  
+    schedule.color = calendar.color;
+    schedule.bgColor = calendar.bgColor;
+    schedule.dragBgColor = calendar.dragBgColor;
+    schedule.borderColor = calendar.borderColor;
+  
+    if (schedule.category === 'milestone') {
+      schedule.color = schedule.bgColor;
+      schedule.bgColor = 'transparent';
+      schedule.dragBgColor = 'transparent';
+      schedule.borderColor = 'transparent';
+    }
+  
+    schedule.raw.memo = chance.sentence();
+    schedule.raw.creator.name = chance.name();
+    schedule.raw.creator.avatar = chance.avatar();
+    schedule.raw.creator.company = chance.company();
+    schedule.raw.creator.email = chance.email();
+    schedule.raw.creator.phone = chance.phone();
+  
+    if (chance.bool({ likelihood: 20 })) {
+      var travelTime = chance.minute();
+      schedule.goingDuration = travelTime;
+      schedule.comingDuration = travelTime;
+    }
+  
+    ScheduleList.push(schedule);
+  }
+
+  function getMySchedules()
+  {
 
     var today = new Date();
   
@@ -388,6 +437,8 @@ var ScheduleList = [];
             timezone: myTimeZone
         }
     });
+
+
   }
   // Events initialize
   function setEventListener() {
