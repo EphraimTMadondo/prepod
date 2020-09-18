2b4a9c6b2ffb63b99f0f9e8dab82830b1156e126
Ephraim Madondo <madondoephraim@gmail.com>
2020-09-18 11:07:34 +0200

    Inner Pages proposals kanban

--- application/views/admin/proposals/pipeline/manage.php
+++ application/views/admin/proposals/pipeline/manage.php
@@ -4,43 +4,44 @@
 <div class="app-content content">
    <div class="content-overlay"></div>
    <div class="content-wrapper">
-        <!-- Basic Kanban App -->
-        <div class="kanban-overlay"></div>
-            <section id="kanban-wrapper" class="mt-2">
-                <div class="row">
-                    <div class="col-md-8">
-                        <?php if(has_permission('proposals','','create')){ ?>
-                        <a href="<?php echo admin_url('proposals/proposal'); ?>" class="btn btn-primary float-left new"><?php echo _l('new_proposal'); ?></a>
-                        <?php } ?>
-                        <a href="<?php echo admin_url('proposals/pipeline/'.$switch_pipeline); ?>" class="btn btn-light ml-1 float-left"><?php echo _l('switch_to_list_view'); ?></a>
-                    </div>
-                    <div class="col-md-4" data-toggle="tooltip" data-placement="bottom" data-title="<?php echo _l('search_by_tags'); ?>">
-                        <?php echo render_input('search','','','search',array('data-name'=>'search','onkeyup'=>'proposals_pipeline();'),array(),'no-margin') ?>
-                        <?php echo form_hidden('sort_type'); ?>
-                        <?php echo form_hidden('sort',(get_option('default_proposals_pipeline_sort') != '' ? get_option('default_proposals_pipeline_sort_type') : '')); ?>
-                    </div>
-                    <?php echo form_hidden('proposalid',$proposalid); ?>
-                    <div class="col-md-12">
-                        <div class="kanban-leads-sort">
-                            <span class="bold"><?php echo _l('proposals_pipeline_sort'); ?>: </span>
-                            <a href="#" onclick="proposal_pipeline_sort('datecreated'); return false" class="datecreated">
-                                <?php if(get_option('default_proposals_pipeline_sort') == 'datecreated'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_datecreated'); ?>
-                                </a>
-                            |
-                            <a href="#" onclick="proposal_pipeline_sort('date'); return false" class="date">
-                            <?php if(get_option('default_proposals_pipeline_sort') == 'date'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_proposal_date'); ?>
-                                </a>
-                            |
-                            <a href="#" onclick="proposal_pipeline_sort('pipeline_order');return false;" class="pipeline_order">
-                                <?php if(get_option('default_proposals_pipeline_sort') == 'pipeline_order'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_pipeline'); ?>
-                                </a>
-                            |
-                            <a href="#" onclick="proposal_pipeline_sort('open_till');return false;" class="open_till">
-                            <?php if(get_option('default_proposals_pipeline_sort') == 'open_till'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_open_till'); ?>
-                            </a>
-                        </div>
-                    </div>
+        <div class="content-header row">
+            <div class="col-md-8">
+                <?php if(has_permission('proposals','','create')){ ?>
+                <a href="<?php echo admin_url('proposals/proposal'); ?>" class="btn btn-primary float-left new"><?php echo _l('new_proposal'); ?></a>
+                <?php } ?>
+                <a href="<?php echo admin_url('proposals/pipeline/'.$switch_pipeline); ?>" class="btn btn-light ml-1 float-left"><?php echo _l('switch_to_list_view'); ?></a>
+            </div>
+            <div class="col-md-4" data-toggle="tooltip" data-placement="bottom" data-title="<?php echo _l('search_by_tags'); ?>">
+                <?php echo render_input('search','','','search',array('data-name'=>'search','onkeyup'=>'proposals_pipeline();'),array(),'no-margin') ?>
+                <?php echo form_hidden('sort_type'); ?>
+                <?php echo form_hidden('sort',(get_option('default_proposals_pipeline_sort') != '' ? get_option('default_proposals_pipeline_sort_type') : '')); ?>
+            </div>
+            <?php echo form_hidden('proposalid',$proposalid); ?>
+            <div class="col-md-12">
+                <div class="kanban-leads-sort">
+                    <span class="bold"><?php echo _l('proposals_pipeline_sort'); ?>: </span>
+                    <a href="#" onclick="proposal_pipeline_sort('datecreated'); return false" class="datecreated">
+                        <?php if(get_option('default_proposals_pipeline_sort') == 'datecreated'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_datecreated'); ?>
+                        </a>
+                    |
+                    <a href="#" onclick="proposal_pipeline_sort('date'); return false" class="date">
+                    <?php if(get_option('default_proposals_pipeline_sort') == 'date'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_proposal_date'); ?>
+                        </a>
+                    |
+                    <a href="#" onclick="proposal_pipeline_sort('pipeline_order');return false;" class="pipeline_order">
+                        <?php if(get_option('default_proposals_pipeline_sort') == 'pipeline_order'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_pipeline'); ?>
+                        </a>
+                    |
+                    <a href="#" onclick="proposal_pipeline_sort('open_till');return false;" class="open_till">
+                    <?php if(get_option('default_proposals_pipeline_sort') == 'open_till'){echo '<i class="kanban-sort-icon fa fa-sort-amount-'.strtolower(get_option('default_proposals_pipeline_sort_type')).'"></i> ';} ?><?php echo _l('proposals_sort_open_till'); ?>
+                    </a>
                 </div>
+            </div>
+        </div>
+        <div class="content-body">
+                <!-- Basic Kanban App -->
+            <div class="kanban-overlay"></div>
+            <section id="kanban-wrapper" class="mt-2">
                 <div class="row">
                     <div class="col-12">
                         <button type="button" class="btn btn-primary mb-1" id="add-kanban">
@@ -143,6 +144,7 @@
                 <!--/ User Chat profile right area -->
             </section>
             <!--/ Sample Project kanban -->
+        </div>
     </div>
 </div>
 <div id="proposal">
