118e9fa2bf2bb50639f9623e1630f0cd037f8f0c
Ephraim Madondo <madondoephraim@gmail.com>
2020-10-06 20:36:38 +0200

    hrm hide stats

--- modules/hrm/views/hrm_dashboard.php
+++ modules/hrm/views/hrm_dashboard.php
@@ -20,7 +20,7 @@
 
     <div class="col-md-12">
       <h4><?php echo _l('hrm_dashboard');?></h4>
-      <button class="float-right btn btn-light ml-1 btn-with-tooltip" data-toggle="stats-top" title="<?php echo _l('view_stats_tooltip'); ?>"><i class="bx bx-bar-chart-alt"></i></button>
+      <a href="#stats-top" class="float-right btn btn-light ml-1 btn-with-tooltip" data-toggle="hide" title="<?php echo _l('view_stats_tooltip'); ?>"><i class="bx bx-bar-chart-alt"></i></a>
       <hr />
     </div>
     <div id="stats-top" class="col-md-12 mt-1 hide">
