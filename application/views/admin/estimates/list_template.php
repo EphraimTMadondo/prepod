b955dd1b5b27b11dcb3beffe176209f8edf6ba35
Ephraim Madondo <madondoephraim@gmail.com>
2020-10-07 18:59:24 +0200

    crm filters estimates

--- application/views/admin/estimates/list_template.php
+++ application/views/admin/estimates/list_template.php
@@ -1,92 +1,19 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="col-md-12">
   <div class="card mb-1 mt-2">
-   <div class="card-body _buttons">
-    <?php $this->load->view('admin/estimates/estimates_top_stats');
-    ?>
-    <?php if(has_permission('estimates','','create')){ ?>
-     <a href="<?php echo admin_url('estimates/estimate'); ?>" class="btn btn-secondary float-left new new-estimate-btn"><?php echo _l('create_new_estimate'); ?></a>
-   <?php } ?>
-   <a href="<?php echo admin_url('estimates/pipeline/'.$switch_pipeline); ?>" class="btn btn-light ml-1 float-left switch-pipeline hidden-xs"><?php echo _l('switch_to_pipeline'); ?></a>
-   <div class="display-block text-right">
-     <div class="btn-group float-right mleft4 btn-with-tooltip-group _filter_data" data-toggle="tooltip" data-title="<?php echo _l('filter_by'); ?>">
-      <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fa fa-filter" aria-hidden="true"></i>
-      </button>
-      <ul class="dropdown-menu width300">
-       <li>
-        <a href="#" data-cview="all" onclick="dt_custom_view('','.table-estimates',''); return false;">
-          <?php echo _l('estimates_list_all'); ?>
-        </a>
-      </li>
-      <li class="divider"></li>
-      <li class="<?php if($this->input->get('filter') == 'not_sent'){echo 'active'; } ?>">
-        <a href="#" data-cview="not_sent" onclick="dt_custom_view('not_sent','.table-estimates','not_sent'); return false;">
-          <?php echo _l('not_sent_indicator'); ?>
-        </a>
-      </li>
-      <li>
-        <a href="#" data-cview="invoiced" onclick="dt_custom_view('invoiced','.table-estimates','invoiced'); return false;">
-          <?php echo _l('estimate_invoiced'); ?>
-        </a>
-      </li>
-      <li>
-        <a href="#" data-cview="not_invoiced" onclick="dt_custom_view('not_invoiced','.table-estimates','not_invoiced'); return false;"><?php echo _l('estimates_not_invoiced'); ?></a>
-      </li>
-      <li class="divider"></li>
-      <?php foreach($estimate_statuses as $status){ ?>
-        <li class="<?php if($this->input->get('status') == $status){echo 'active';} ?>">
-          <a href="#" data-cview="estimates_<?php echo $status; ?>" onclick="dt_custom_view('estimates_<?php echo $status; ?>','.table-estimates','estimates_<?php echo $status; ?>'); return false;">
-            <?php echo format_estimate_status($status,'',false); ?>
-          </a>
-        </li>
+    <div class="card-body _buttons">
+      <?php $this->load->view('admin/estimates/estimates_top_stats');
+      ?>
+      <?php if(has_permission('estimates','','create')){ ?>
+      <a href="<?php echo admin_url('estimates/estimate'); ?>" class="btn btn-primary new new-estimate-btn"><?php echo _l('create_new_estimate'); ?></a>
       <?php } ?>
-      <div class="clearfix"></div>
-
-      <?php if(count($estimates_sale_agents) > 0){ ?>
-        <div class="clearfix"></div>
-        <li class="divider"></li>
-        <li class="dropdown-submenu float-left">
-          <a href="#" tabindex="-1"><?php echo _l('sale_agent_string'); ?></a>
-          <ul class="dropdown-menu dropdown-menu-left">
-           <?php foreach($estimates_sale_agents as $agent){ ?>
-             <li>
-              <a href="#" data-cview="sale_agent_<?php echo $agent['sale_agent']; ?>" onclick="dt_custom_view(<?php echo $agent['sale_agent']; ?>,'.table-estimates','sale_agent_<?php echo $agent['sale_agent']; ?>'); return false;"><?php echo $agent['full_name']; ?>
-            </a>
-          </li>
-        <?php } ?>
-      </ul>
-    </li>
-  <?php } ?>
-  <div class="clearfix"></div>
-  <?php if(count($estimates_years) > 0){ ?>
-    <li class="divider"></li>
-    <?php foreach($estimates_years as $year){ ?>
-      <li class="active">
-        <a href="#" data-cview="year_<?php echo $year['year']; ?>" onclick="dt_custom_view(<?php echo $year['year']; ?>,'.table-estimates','year_<?php echo $year['year']; ?>'); return false;"><?php echo $year['year']; ?>
-      </a>
-    </li>
-  <?php } ?>
-<?php } ?>
-</ul>
-</div>
-<a href="#" class="btn btn-light btn-with-tooltip estimates-total" onclick="slideToggle('#stats-top'); init_estimates_total(true); return false;" data-toggle="tooltip" title="<?php echo _l('view_stats_tooltip'); ?>"><i class="bx bx-bar-chart"></i></a>
-</div>
-</div>
-</div>
-<div class="row">
-  <div class="col-md-12" id="small-table">
-    <div class="card">
-      <div class="card-body">
+      <a href="<?php echo admin_url('estimates/pipeline/'.$switch_pipeline); ?>" class="btn btn-primary switch-pipeline hidden-xs"><?php echo _l('switch_to_pipeline'); ?></a>
+      <a href="#" class="btn btn-primary float-right btn-with-tooltip estimates-total" onclick="slideToggle('#stats-top'); init_estimates_total(true); return false;" data-toggle="tooltip" title="<?php echo _l('view_stats_tooltip'); ?>"><i class="bx bx-bar-chart"></i></a>
+    </div>
+    <div class="card-body">
         <!-- if estimateid found in url -->
         <?php echo form_hidden('estimateid',$estimateid); ?>
         <?php $this->load->view('admin/estimates/table_html'); ?>
       </div>
-    </div>
   </div>
-  <div class="col-md-7 small-table-right-col">
-    <div id="estimate" class="hide">
-    </div>
-  </div>
-</div>
 </div>
--- application/views/admin/estimates/manage.php
+++ application/views/admin/estimates/manage.php
@@ -13,6 +13,12 @@
 <script>var hidden_columns = [2,5,6,8,9];</script>
 <?php init_tail(); ?>
 <script>
+   function custom_view(){
+      var view = $('#select-filter').val();
+      console.log(view);
+      dt_custom_view(view,'.table-estimates',view);
+   }
+
 	$(function(){
 		init_estimate();
 	});
