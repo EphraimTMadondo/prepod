bcca9acb6296990361a56fb54d128ed73d439efc
Ephraim Madondo <madondoephraim@gmail.com>
2020-10-07 21:38:22 +0200

    crm filters projects

--- application/views/admin/projects/manage.php
+++ application/views/admin/projects/manage.php
@@ -35,7 +35,7 @@
                 }
                 ?>
               </div>
-              <div class="col-md-12 stats-top hide mt-1 row _filters _hidden_inputs center">
+              <div class="col-md-12 stats-top hide mt-1 _filters _hidden_inputs center">
                 <?php
                 echo form_hidden('my_projects');
                 foreach($statuses as $status){
