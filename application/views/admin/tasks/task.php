a88fc44fe9f2d053d48f406621378a5cd22133d5
itsvictorj <janhivictor4@gmail.com>
2020-10-15 12:13:10 +0200

    addeed task modal view 25

--- application/views/admin/tasks/task.php
+++ application/views/admin/tasks/task.php
@@ -24,7 +24,7 @@
          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
          <h4 class="modal-title" id="myModalLabel" style ="color:white;">
             <?php echo $title; ?>
-            66
+            
          </h4>
       </div>
       <div class="modal-body">
