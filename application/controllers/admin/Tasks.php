d7bd18743e21febd0f53ee58cc1abcc6e8c165d7
itsvictorj <janhivictor4@gmail.com>
2020-10-16 14:49:28 +0200

    add task model selectpicker class removed 8

--- application/controllers/admin/Tasks.php
+++ application/controllers/admin/Tasks.php
@@ -331,11 +331,11 @@ class Tasks extends AdminController
                     }
                 }
             
-              //  echo json_encode([
+               echo json_encode([
                     'success' => $success,
                     'id'      => $_id,
                     'message' => $message,
-               // ]);
+                ]);
             } else {
                 if (!has_permission('tasks', '', 'edit')) {
                     header('HTTP/1.0 400 Bad error');
