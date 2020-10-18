936143bd2fd09359d8601da73a641c2557269d4a
itsvictorj <janhivictor4@gmail.com>
2020-10-18 12:06:17 +0200

    updated css file 35

--- assets/js/main.js
+++ assets/js/main.js
@@ -5241,8 +5241,8 @@ function showHideTagsPlaceholder($tagit) {
 }
 
 // Generate float alert
-
-function alert_float(type, message, timeout) {
+/**
+ * function alert_float(type, message, timeout) {
     //alert("running in main.js");
     var aId, el;
 
@@ -5270,6 +5270,7 @@ function alert_float(type, message, timeout) {
     }, timeout);
 }
 
+*/
 
 // Create new task directly from relation, related options selected after modal is shown
 function new_task_from_relation(table, rel_type, rel_id) {
