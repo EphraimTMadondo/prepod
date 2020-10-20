cc6a3773f8aa4c0b05825948808aa4b99e52d290
itsvictorj <janhivictor4@gmail.com>
2020-10-20 11:04:03 +0200

    script added vendors-admin

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -6,6 +6,9 @@
 <script src="<?php echo base_url();?>assets/frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.min.js"></script>
 <script src="<?php echo base_url();?>assets/frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js"></script>
 
+<script src="<?php echo base_url();?>assets/builds/vendor-admin.js"></script>
+
+
 <!-- Datepicker -->
 <?php
    echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/pickers/pickadate/picker.js'></script>\n";
