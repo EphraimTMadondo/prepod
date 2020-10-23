86447f59a3a4b799ac3f254abbc6014fc3b81332
itsvictorj <janhivictor4@gmail.com>
2020-10-23 13:50:33 +0200

    added echo test staff model

--- application/models/Staff_model.php
+++ application/models/Staff_model.php
@@ -541,7 +541,7 @@ public function test2()
         
         $data2['company_username'] = $_SESSION['current_company'];
          $this->db->insert(db_prefix() . 'staff_companies', $data2);
-         print_r($data2);
+         print_r($data);
         if ($staffid) {
              
             $slug = $data['firstname'] . ' ' . $data['lastname'];
