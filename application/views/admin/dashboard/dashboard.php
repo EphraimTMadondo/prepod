86c6e5b5194a885e78d52c991c79737d85032758
itsvictorj <janhivictor4@gmail.com>
2020-10-23 10:40:52 +0200

    dashboard look 3

--- application/views/admin/dashboard/dashboard.php
+++ application/views/admin/dashboard/dashboard.php
@@ -21,7 +21,6 @@
             </div>
             <!-- flex -->
             
-            <div style = "display: flex">
             <?php hooks()->do_action('after_dashboard_half_container'); ?>
             <div class="col-md-8" data-container="left-8">
                 <?php render_dashboard_widgets('left-8'); ?>
@@ -31,8 +30,7 @@
             <div class="col-md-4" data-container="right-4">
                 <?php  render_dashboard_widgets('right-4'); ?>
             </div>
-            </div>
-            </div>
+     
             <!---flex end -->
             <div class="clearfix"></div>
             <div class="col-md-4" data-container="bottom-left-4">
