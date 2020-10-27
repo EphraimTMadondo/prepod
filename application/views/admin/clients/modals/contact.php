a2dfea7a74aa2c8c36aaacb4ea2db9b413779a11
itsvictorj <janhivictor4@gmail.com>
2020-10-27 14:35:31 +0200

    cobtact modal

--- application/views/admin/clients/modals/contact.php
+++ application/views/admin/clients/modals/contact.php
@@ -3,6 +3,49 @@
 <div class="modal fade" id="contact" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
+        <style>
+            .modal .modal-content .modal-header {
+            align-items: center;
+        }
+        
+                .modal-header {
+            display: block;
+            background: #226faa;
+            padding: 15px 30px;
+        }
+        
+        .modal-body {
+    position: relative;
+    padding: 15px;
+}
+.bootstrap-select>select {
+    position: absolute!important;
+    bottom: 0;
+    left: 50%;
+    display: block!important;
+    width: .5px!important;
+    height: 100%!important;
+    padding: 0!important;
+    opacity: 0!important;
+    border: none;
+    z-index: 0!important;
+}
+
+.bootstrap-select .btn-default {
+    background: #fff!important;
+    color: #415164!important;
+    border: 1px solid #bfcbd9!important;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    padding: 4px 10px;
+    line-height: 2;
+    height: 36px;
+    text-transform: inherit;
+    padding-left: 10px;
+}
+                                        
+</style>
+
             <?php echo form_open(admin_url('clients/form_contact/'.$customer_id.'/'.$contactid),array('id'=>'contact-form','autocomplete'=>'off')); ?>
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
