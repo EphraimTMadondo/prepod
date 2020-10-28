93a97a61e8b64b9175f85c3c6aa5e96e87ae2a4b
itsvictorj <janhivictor4@gmail.com>
2020-10-28 08:41:55 +0200

    contacts page 6

--- application/views/admin/clients/tabs.php
+++ application/views/admin/clients/tabs.php
@@ -9,6 +9,7 @@ style =
     -webkit-box-shadow: 0 1px 15px 1px rgba(90,90,90,.08);
     flex-wrap: nowrap;
     display: block;
+    background: transparent;
 
 "
 
@@ -16,7 +17,10 @@ style =
   <?php
   foreach(filter_client_visible_tabs($customer_tabs) as $key => $tab){
     ?>
-    <li class="nav-item">
+    <li class="nav-item" style = "border-bottom: 1px solid #e5e5e5;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: rgb(229, 229, 229);">
       <a class="nav-link <?php if($key == 'profile'){echo 'active ';} ?>" data-group="<?php echo $key; ?>" href="<?php echo admin_url('clients/client/'.$client->userid.'?group='.$key); ?>">
         <?php if(!empty($tab['icon'])){ ?>
             <i class="<?php echo $tab['icon']; ?> menu-icon" aria-hidden="true"></i>
