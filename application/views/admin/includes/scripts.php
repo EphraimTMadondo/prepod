f5a8b03eeb85398f0d69d3fc75d55901aad2ea80
itsvictorj <janhivictor4@gmail.com>
2020-11-02 13:22:28 +0200

    rem ven

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -116,7 +116,7 @@
 <script src="<?php echo base_url();?>assets/js/app.js"></script>
 <script src="<?php echo base_url();?>assets/plugins/datetimepicker/jquery.datetimepicker.full.js"></script>
 <script src="<?php echo base_url();?>application/views/themes/assets/plugins/accounting.js/accounting.js"></script>
-<script src="<?php echo base_url();?>application/views/themes/assets/builds/vendor-admin.js"></script>
+
 <script src="<?php echo base_url();?>application/views/themes/assets/plugins/internal/validation/app-form-validation.js"></script>
 <script src="<?php echo base_url();?>assets/builds/common.js"></script>
 <script src="<?php echo base_url();?>assets/plugins/internal/validation/app-form-validation.js"></script>
