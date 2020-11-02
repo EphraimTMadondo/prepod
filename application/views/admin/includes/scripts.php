c914c1dee2848b92cc84066a1c1d891acba873fc
itsvictorj <janhivictor4@gmail.com>
2020-11-02 12:45:55 +0200

    added vendro-admin 3

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -117,6 +117,8 @@
 <script src="<?php echo base_url();?>assets/plugins/datetimepicker/jquery.datetimepicker.full.js"></script>
 <script src="<?php echo base_url();?>application/views/themes/assets/plugins/accounting.js/accounting.js"></script>
 <script src="<?php echo base_url();?>application/views/themes/assets/builds/vendor-admin.js"></script>
+<script src="<?php echo base_url();?>application/views/themes/assets/plugins/internal/validation/app-form-validation.js"></script>
+
 
 
 
