c52c9fc3e99964197415b72c21d90603310e6c2f
itsvictorj <janhivictor4@gmail.com>
2020-11-04 11:54:08 +0200

    sremioved scripts bottom 2

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -26,10 +26,10 @@
 
 <script type="text/javascript" id="tinymce-js" src="<?php echo base_url();?>assets/plugins/tinymce/tinymce.min.js?v=2.4.2"></script>
 <script type="text/javascript" id="jquery-shortcuts-js" src="<?php echo base_url();?>assets/plugins/jquery-shortcuts/jquery.shortcuts.js"></script>
-<!---
+
 <script type="text/javascript" id="jquery-validation-js" src="<?php echo base_url();?>assets/plugins/jquery-validation/jquery.validate.min.js?v=2.4.2"></script>
 
-
+<!---
 <script type="text/javascript" id="google-js" src="https://apis.google.com/js/api.js?onload=onGoogleApiLoad" defer></script>
 <script type="text/javascript" id="common-js" src="<?php echo base_url();?>assets/builds/common.js"></script>
 <script type="text/javascript" id="app-js" src="<?php echo base_url();?>assets/js/main.js"></script>
