fb0740d9036d68937dac0f11aea633afdfce31cc
itsvictorj <janhivictor4@gmail.com>
2020-11-05 10:00:05 +0200

    scripts step back to sremioved j

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -26,10 +26,7 @@
 
 <script type="text/javascript" id="tinymce-js" src="<?php echo base_url();?>assets/plugins/tinymce/tinymce.min.js?v=2.4.2"></script>
 <script type="text/javascript" id="jquery-shortcuts-js" src="<?php echo base_url();?>assets/plugins/jquery-shortcuts/jquery.shortcuts.js"></script>
-<!--
 <script type="text/javascript" id="jquery-validation-js" src="<?php echo base_url();?>assets/plugins/jquery-validation/jquery.validate.min.js?v=2.4.2"></script>
-
--->
 <script type="text/javascript" id="google-js" src="https://apis.google.com/js/api.js?onload=onGoogleApiLoad" defer></script>
 <script type="text/javascript" id="common-js" src="<?php echo base_url();?>assets/builds/common.js"></script>
 <script type="text/javascript" id="app-js" src="<?php echo base_url();?>assets/js/main.js"></script>
@@ -106,8 +103,6 @@
 <script src="<?php echo base_url();?>modules/hrm/assets/js/allowancetype.js"></script>
 <script src="<?php echo base_url();?>modules/hrm/assets/js/contract.js"></script>
 <script src="<?php echo base_url();?>modules/hrm/assets/js/contracttype.js"></script>
-<script src="<?php echo base_url();?>modules/assets/js/proposal.js"></script>
-
 <script src="<?php echo base_url();?>modules/hrm/assets/js/jobposition.js"></script>
 <script src="<?php echo base_url();?>modules/hrm/assets/js/managecontract.js"></script>
 <script src="<?php echo base_url();?>modules/hrm/assets/js/managesettings.js"></script>
