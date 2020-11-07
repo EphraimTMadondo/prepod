9c6d9bddf79be4afaa0dfa09059caa0776d74945
itsvictorj <janhivictor4@gmail.com>
2020-11-07 13:56:02 +0200

    get clients 15

--- application/helpers/relation_helper.php
+++ application/helpers/relation_helper.php
@@ -16,12 +16,12 @@ function get_relation_data($type, $rel_id = '')
         $q = $CI->input->post('q');
         $q = trim($q);
     }
-    $companyusername =  $_SESSION['current_company'];
+
     $data = [];
     if ($type == 'customer' || $type == 'customers') {
         $where_clients = '';
         if ($q) {
-            $where_clients .= '(company LIKE "%' . $CI->db->escape_like_str($q) . '%" ESCAPE \'!\' OR CONCAT(firstname, " ", lastname) LIKE "%' . $CI->db->escape_like_str($q) . '%" ESCAPE \'!\' OR email LIKE "%' . $CI->db->escape_like_str($q) . '%" ESCAPE \'!\') AND ' . db_prefix() . "clients.company_username ="."'tbga'";
+            $where_clients .= '(company LIKE "%' . $CI->db->escape_like_str($q) . '%" ESCAPE \'!\' OR CONCAT(firstname, " ", lastname) LIKE "%' . $CI->db->escape_like_str($q) . '%" ESCAPE \'!\' OR email LIKE "%' . $CI->db->escape_like_str($q) . '%" ESCAPE \'!\') AND ' . db_prefix() . 'clients.active = 1';
         }
 
         $data = $CI->clients_model->get($rel_id, $where_clients);
