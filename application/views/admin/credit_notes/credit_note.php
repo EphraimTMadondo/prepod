bff41a1cf17afcbe5b6404b0a129037f31709fa5
itsvictorj <janhivictor4@gmail.com>
2020-11-08 15:30:47 +0200

    rem og number 16

--- application/views/admin/credit_notes/credit_note.php
+++ application/views/admin/credit_notes/credit_note.php
@@ -273,7 +273,9 @@ textarea.form-control {
               }
             }
            // $_invoice_number = str_pad(, get_option('number_padding_prefixes'), '0', STR_PAD_LEFT);
+   
 
+          echo $_credit_note_number;
             $_credit_note_number = str_pad($last, get_option('number_padding_prefixes'), '0', STR_PAD_LEFT);
             $isedit = isset($credit_note) ? 'true' : 'false';
             $data_original_number = $_credit_note_number;
