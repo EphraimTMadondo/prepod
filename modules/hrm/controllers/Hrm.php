be915215818dc0a9340edcd7b028ebce7c698065
itsvictorj <janhivictor4@gmail.com>
2020-11-09 09:55:29 +0200

    contract psge 5

--- modules/hrm/controllers/Hrm.php
+++ modules/hrm/controllers/Hrm.php
@@ -821,6 +821,7 @@ public function latch_payslip(){
                     access_denied('hrm');
                 }
                 $id = $this->hrm_model->add_contract($data);
+                echo "running";
                 if ($id) {
                     set_alert('success', _l('added_successfully', _l('contract')));
                   //  redirect(admin_url('hrm/contract/' . $id));
