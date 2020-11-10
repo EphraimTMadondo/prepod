bbc29f6f1868a9ca44d301124a3a848b155bf40c
itsvictorj <janhivictor4@gmail.com>
2020-11-10 12:22:28 +0200

    NO EDIT BANK 12

--- application/views/admin/tables/roles.php
+++ application/views/admin/tables/roles.php
@@ -23,10 +23,25 @@ foreach ($rResult as $aRow) {
     for ($i = 0; $i < count($aColumns); $i++) {
         $_data = $aRow[$aColumns[$i]];
         if ($aColumns[$i] == 'name') {
+
+           
+            if($aRow['company_username'] == $companyusername)
+            {
+
             $_data            = '<a href="' . admin_url('roles/role/' . $aRow['roleid']) . '" class="mb-1 display-block">' . $_data . '</a>';
             $_data .= '<span class="mtop10 display-block">' . _l('roles_total_users') . ' ' . total_rows(db_prefix().'staff', [
                 'role' => $aRow['roleid'],
                 ]) . '</span>';
+            }
+            else
+            {
+                $_data            = '<a href="'  '" class="mb-1 display-block">' . $_data . '</a>';
+                $_data .= '<span class="mtop10 display-block">' . _l('roles_total_users') . ' ' . total_rows(db_prefix().'staff', [
+                    'role' => $aRow['roleid'],
+                    ]) . '</span>';
+            }
+
+
         }
         $row[] = $_data;
     }
