e1886469fb6f3362ec7c91988fc9b41185373b7f
itsvictorj <janhivictor4@gmail.com>
2020-11-11 15:16:04 +0200

    get options new testing val 41

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,24 @@ hr dash top button
 whole project /#1 filter access
 datepicker working but classes missing?
 
-20 15 15 15
+20 15 15 15 15
+
+
+Working with Beki 'Butch Cassidy' Mbambo is a refreshing experience. His willingness to consider and deliver my needs as a client is 
+matched by his willingness to offer advice on bringing about the best vocal performace. In all my experiences with him I recieve not 
+only a mixed and mastered product but some creative additions as well which usually transformed the song to a
+great place I would have never imagined on my own. I believe these strengths come from his experiences as an artist himself.
+
+
+Right There
+
+I seee you glow in the sunlight
+You're a song that I wrote that I still write 
+That means the love don't pass
+As the sand falls down the hour-glass
+Let's take a cruise don't crash
+Memories in my bag I unpack from the past
+All I really want is now
+The present moment I notice is ours
+Sun goes and the wind blows
+
