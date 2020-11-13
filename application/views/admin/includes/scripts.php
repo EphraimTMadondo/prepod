55ca25db3761e4f8effdf73430f6e69a31f483f7
itsvictorj <janhivictor4@gmail.com>
2020-11-13 13:51:32 +0200

    added main.js from themes

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -130,6 +130,10 @@
 <script src="<?php echo base_url();?>application/views/themes/assets/plugins/accounting.js/accounting.js"></script>
 <script src="<?php echo base_url();?>application/views/assets/builds/vendor-admin.js"></script>
 
+<script src="<?php echo base_url();?>application/views/themes/assets/js/main.js"></script>
+
+
+
 
 
 
