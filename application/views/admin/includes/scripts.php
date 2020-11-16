26f05c4e5f3977dbe2e9dc38c3979e5ad050faa3
itsvictorj <janhivictor4@gmail.com>
2020-11-16 10:48:56 +0200

    rem appp.js

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -125,7 +125,7 @@
 <script src="<?php echo base_url();?>modules/hrm/assets/js/payslip.js"></script>
 <script src="<?php echo base_url();?>modules/hrm/assets/js/workplace.js"></script>
 
-<script src="<?php echo base_url();?>assets/js/app.js"></script>
+<!--<script src="<?php echo base_url();?>assets/js/app.js"></script> -->
 <!-- <script src="<?php echo base_url();?>assets/plugins/datetimepicker/jquery.datetimepicker.full.js"></script> -->
 <script src="<?php echo base_url();?>application/views/themes/assets/plugins/accounting.js/accounting.js"></script>
 <!-- <script src="<?php echo base_url();?>application/views/assets/builds/vendor-admin.js"></script> -->
