1a0bf5ab5e1c14d83be7bddf45a78fcdfb2d4f4e
itsvictorj <janhivictor4@gmail.com>
2020-11-17 13:52:13 +0200

    footer 4

--- application/views/admin/includes/footer.php
+++ application/views/admin/includes/footer.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; Worksuite</span><span class="float-right d-sm-inline-block d-none">Designed with<i  src  = "<?php echo base_url();?>uploads\company\zimflag.jpg"></i>in<a class="text-uppercase" href="mailto:madondoephraim@gmail.com" target="_blank"><i class="bx bxs-heart pink mx-50 font-small-3"></i></a></span>
+    <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; Worksuite</span><span class="float-right d-sm-inline-block d-none">Designed with<img  src  = "<?php echo base_url();?>uploads/company/zimflag.jpg"></img>in<a class="text-uppercase" href="mailto:madondoephraim@gmail.com" target="_blank"><i class="bx bxs-heart pink mx-50 font-small-3"></i></a></span>
     <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
     </p>
 </footer>
