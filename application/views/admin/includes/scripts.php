1f7bb56a327044ac9044d1b9573148c71fc71540
itsvictorj <janhivictor4@gmail.com>
2020-11-18 15:07:13 +0200

    script back 7

--- application/views/admin/includes/scripts.php
+++ application/views/admin/includes/scripts.php
@@ -1,21 +1,18 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php include_once(APPPATH.'views/admin/includes/helpers_bottom.php'); ?>
- <!-- BEGIN: Vendor JS-->
- <script src="https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/vendors.min.js"></script>
+<!-- BEGIN: Vendor JS-->
+<script src="https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/vendors.min.js"></script>
 <script src="https://worksuite.app/prepod/assets/frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.min.js"></script>
 <script src="https://worksuite.app/prepod/assets/frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.min.js"></script>
 <script src="https://worksuite.app/prepod/assets/frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js"></script>
+
 <!-- Datepicker -->
-<?php
-   echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/pickers/pickadate/picker.js'></script>\n";
-   echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/pickers/pickadate/legacy.js'></script>\n";
-   echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/pickers/pickadate/picker.date.js'></script>\n";
-   echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/pickers/pickadate/picker.time.js'></script>\n";
-   echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/pickers/daterange/moment.min.js'></script>\n";
-   echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/pickers/daterange/daterangepicker.js'></script>\n";
-?>
-
-
+<script src='https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/pickers/pickadate/picker.js'></script>
+<script src='https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/pickers/pickadate/legacy.js'></script>
+<script src='https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/pickers/pickadate/picker.date.js'></script>
+<script src='https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/pickers/pickadate/picker.time.js'></script>
+<script src='https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/pickers/daterange/moment.min.js'></script>
+<script src='https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/pickers/daterange/daterangepicker.js'></script>
 
 <!-- Data tables -->
 <script src="https://worksuite.app/prepod/assets/frest/app-assets/vendors/js/tables/datatable/datatables.min.js"></script>
@@ -30,67 +27,14 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
 <script src="https://worksuite.app/prepod/assets/plugins/bootstrap-select-ajax/js/ajax-bootstrap-select.min.js"></script>
 
-<script src="<?php echo base_url();?>application/views/themes/assets/plugins/moment-timezone/moment-timezone-with-data-10-year-range.js"></script>
-<?php
-   switch($page)
-   {
-      case 'dashboard':
-     
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/charts/apexcharts.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/extensions/dragula.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/extensions/swiper.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/charts/chart.min.js'></script>\n";
 
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-calendar.min.js'></script>\n";
-   
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/extensions/moment.min.js'></script>\n";
-         echo "<script src='".base_url()."application/views/themes/assets/js/dashboard-custom.js'></script>\n";
+<script type="text/javascript" id="tinymce-js" src="https://worksuite.app/prepod/assets/plugins/tinymce/tinymce.min.js?v=2.4.2"></script>
+<script type="text/javascript" id="jquery-shortcuts-js" src="https://worksuite.app/prepod/assets/plugins/jquery-shortcuts/jquery.shortcuts.js"></script>
+<script type="text/javascript" id="jquery-validation-js" src="https://worksuite.app/prepod/assets/plugins/jquery-validation/jquery.validate.min.js?v=2.4.2"></script>
+<script type="text/javascript" id="google-js" src="https://apis.google.com/js/api.js?onload=onGoogleApiLoad" defer></script>
+<script type="text/javascript" id="common-js" src="https://worksuite.app/prepod/assets/builds/common.js"></script>
+<script type="text/javascript" id="app-js" src="https://worksuite.app/prepod/assets/js/main.js"></script>
 
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-time-picker.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-date-picker.min.js'></script>\n";
-      
-       
-      
-      break;
-      case 'calendar':
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-code-snippet.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-dom.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-time-picker.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-date-picker.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/extensions/moment.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/chance.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/calendar/tui-calendar.min.js'></script>\n";
-      break;
-      case "task_kanban":
-      case "proposals_kanban":
-      case "leads":
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/jkanban/jkanban.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/editors/quill/quill.min.js'></script>\n";
-      break;
-      case "task_list":
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/editors/quill/quill.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/extensions/dragula.min.js'></script>\n";
-      break;
-      case "mailbox":
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/editors/quill/quill.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/extensions/toastr.min.js'></script>\n";
-      break;
-      case "contracts":
-      case "projects":
-      case "reports":
-      case "staff":
-         echo "<script src='".base_url()."assets/plugins/jquery-circle-progress/circle-progress.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/vendors/js/charts/chart.min.js'></script>\n";
-      break;
-      case "hrm":
-      case "purchase":
-      case "staff":
-         echo "<script src='".base_url()."modules/assets/plugins/handsontable/handsontable.full.min.js'></script>\n";
-      break;
-      default:
-      break;
-   }
-?>
 <!-- BEGIN Vendor JS-->
 
 <!-- BEGIN: Theme JS-->
@@ -100,41 +44,10 @@
 <script src="https://worksuite.app/prepod/assets/frest/app-assets/js/core/app.js"></script>
 <script src="https://worksuite.app/prepod/assets/frest/app-assets/js/scripts/components.min.js"></script>
 <script src="https://worksuite.app/prepod/assets/frest/app-assets/js/scripts/footer.min.js"></script>
-
-
-
 <!-- END: Theme JS-->
 
 <!-- BEGIN: Page JS-->
-<?php
-   switch($page)
-   {
-      case 'dashboard':
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/pages/dashboard-ecommerce.min.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/cards/widgets.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/extensions/calendar/calendars-data.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/extensions/calendar/schedules.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/extensions/calendar/app-calendar.js'></script>\n";
-      break;
-      case 'calendar':
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/extensions/calendar/calendars-data.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/extensions/calendar/schedules.js'></script>\n";
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/extensions/calendar/app-calendar.js'></script>\n";
-      break;
-      case "task_list":
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/pages/app-todo.js'></script>\n";
-      break;
-      case "mailbox":
-         echo "<script src='".base_url()."assets/frest/app-assets/js/scripts/pages/app-email.js'></script>\n";
-      break;
-      case "projects":
-         echo "<script src='".base_url()."assets/js/projects.js'></script>\n";
-      break;
-      default:
-      break;
-   }
-?>
-
+<!-- END: Page JS-->
 <script>
         function custom_fields_hyperlink(){
          var cf_hyperlink = $('body').find('.cf-hyperlink');
